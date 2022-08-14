5f915f38ec9b2f28bda884a854f4e7b801cd512c
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2022-08-14 11:29:37 +0600

    modifyed rule3 file with (a * b) codes

--- 01-Big-O/rule3.js
+++ 01-Big-O/rule3.js
@@ -9,3 +9,17 @@ function compressBoxesTwice(boxes, boxes2) {
 }
 
 // BIG O(a + b)
+
+//Log all pairs of array
+const boxes = ['a', 'b', 'c', 'd', 'e'];
+function logAllPairsOfArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            console.log(array[i], array[j])
+        }
+    }
+}
+
+logAllPairsOfArray(boxes)
+
+// BIG O(a * b)
