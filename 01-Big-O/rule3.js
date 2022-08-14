285f92a3160b1ecf71373771489994a546ecaa64
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2022-08-14 11:21:41 +0600

    added rule3 file

---
+++ 01-Big-O/rule3.js
@@ -0,0 +1,11 @@
+function compressBoxesTwice(boxes, boxes2) {
+    boxes.forEach(element => {
+        console.log(element);
+    });
+
+    boxes2.forEach(element => {
+        console.log(element);
+    });
+}
+
+// BIG O(a + b)
